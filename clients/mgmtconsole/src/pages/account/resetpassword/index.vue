d58f81d0df88e75cab5ce060a1623826f4c84cb9
Raphael Bühringer <raphael.buehringer@revanced.com>
2020-12-05 12:08:05 +0100

    add reset password page

---
+++ clients/mgmtconsole/src/pages/account/resetpassword/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="account-page-container">
+    <div class="account-container">
+      <div class="account-company-logo-container">
+        <VLogo class="account-company-logo" />
+      </div>
+      <h1 class="account-title">Reset Password</h1>
+      <form>
+        <section class="account-section">
+          <input
+            class="account-input"
+            type="email"
+            placeholder="E-Mail"
+            required
+          />
+        </section>
+        <section class="account-section">
+          <input
+            class="account-input"
+            type="password"
+            placeholder="New Password"
+            required
+          />
+          <input
+            class="account-input"
+            type="password"
+            placeholder="Verify Password"
+            required
+          />
+        </section>
+        <label class="account-error-message"></label>
+        <button class="account-button" type="submit">Reset</button>
+      </form>
+    </div>
+    <n-link class="account-link" to="/account/signin" prefetch
+      >Back to Sign In Page</n-link
+    >
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export default class AccountResetPasswordPage extends Vue {
+  protected layout() {
+    return 'account';
+  }
+}
+</script>
+
+<style src="~/assets/styles/account.css" scoped></style>
