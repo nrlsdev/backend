e8397869ff39431f4cddaa516ba03c32e99d7671
Raphael Bühringer <raphael.buehringer@revanced.com>
2020-12-09 18:20:05 +0100

    add system server

--- services/system/systemgatewayhttp/src/index.ts
+++ services/system/systemgatewayhttp/src/index.ts
@@ -2,7 +2,9 @@ import { Server } from '@backend/server';
 import { authenticationRouter } from './authentication/authentication-router';
 
 const authenticationServer: Server = new Server('', 8082);
+const systemServer: Server = new Server('', 8083);
 
 authenticationServer.Application.use('/auth', authenticationRouter);
 
 authenticationServer.start();
+systemServer.start();
