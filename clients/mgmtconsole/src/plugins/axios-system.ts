ac12c5539b3fcd8ddc1d16ace1c1b582acef5d6e
Raphael Bühringer <raphael.buehringer@revanced.com>
2020-12-14 20:57:29 +0100

    refresh on 403

--- clients/mgmtconsole/src/plugins/axios-system.ts
+++ clients/mgmtconsole/src/plugins/axios-system.ts
@@ -1,6 +1,8 @@
 import { initializeSystemAPI } from '@/utils/axios-accessor';
 import { Context } from '@nuxt/types';
-import { AxiosInstance } from 'axios';
+import { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import { refreshToken } from '@/api/system-user-authentication';
+import { parse } from 'cookie';
 
 function axiosSystem(
   context: Context,
@@ -14,7 +16,39 @@ function axiosSystem(
   systemAPI.defaults.validateStatus = (statusCode: number) => {
     return statusCode !== 401 && statusCode !== 403;
   };
+
   systemAPI.defaults.headers['Content-Language'] = context.app.i18n.locale;
+
+  // ToDo: Refactor. Do not set httpOnly cookie this way!
+  systemAPI.interceptors.response.use(
+    (response: AxiosResponse) => {
+      return response;
+    },
+    async (error: AxiosError) => {
+      const { response } = error;
+      const { config } = error;
+
+      if (!response || !config) {
+        return Promise.reject(error);
+      }
+
+      config.data = config.data === undefined ? {} : config.data;
+
+      if (response!.status === 403 && !config.data.retry) {
+        config.data.retry = true;
+        await refreshToken(context);
+
+        const setCookies = context.res.getHeaders()['set-cookie'] as string[];
+        const { token } = parse(setCookies[0]);
+
+        config.headers.cookie += `; token=${token}`;
+
+        return systemAPI(config);
+      }
+
+      return Promise.reject(error);
+    },
+  );
 
   inject('systemAPI', systemAPI);
   initializeSystemAPI(systemAPI, context);
