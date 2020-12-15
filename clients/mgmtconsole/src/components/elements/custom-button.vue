a7240eb1a4bebba7ca7a2396c98783abeaadb6ba
Raphael Bühringer <raphael.buehringer@revanced.com>
2020-12-15 20:41:10 +0100

    implement custom button component

---
+++ clients/mgmtconsole/src/components/elements/custom-button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="custom-button-content">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export default class CustomButton extends Vue {}
+</script>
+
+<style src="~/assets/styles/elements/custom-button.css" scoped></style>
+
+<style scoped>
+.custom-button-content {
+  font-weight: bold;
+}
+</style>
