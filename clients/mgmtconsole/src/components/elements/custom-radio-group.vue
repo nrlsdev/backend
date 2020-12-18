0e9fa26880552d49b2171f5c121324e0d27bd062
Raphael Bühringer <raphael.buehringer@revanced.com>
2020-12-18 18:50:26 +0100

    implement radio button and radio group

---
+++ clients/mgmtconsole/src/components/elements/custom-radio-group.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-form-group>
+    <slot />
+  </b-form-group>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export default class CustomRadioGroup extends Vue {}
+</script>
+
+<style scoped></style>
