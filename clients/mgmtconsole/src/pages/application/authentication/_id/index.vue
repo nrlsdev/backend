f77a6a34ff9e54059740bcc207bcbac1036cd255
Raphael Bühringer <raphael.buehringer@revanced.com>
2020-12-27 10:28:36 +0100

    add authentication method collapseables

--- clients/mgmtconsole/src/pages/application/authentication/_id/index.vue
+++ clients/mgmtconsole/src/pages/application/authentication/_id/index.vue
@@ -1,5 +1,50 @@
 <template>
-  <div></div>
+  <div>
+    <h1 class="system-large-title-font">{{ $t('StrAuthentication') }}</h1>
+    <section class="application-section">
+      <div>
+        <h2 class="system-title-two-font">
+          {{ $t('StrAuthenticationMethods') }}
+        </h2>
+      </div>
+      <div class="application-general-info-container">
+        <Collapse
+          icon="email"
+          :title="$t('StrEmailAndPassword')"
+          statusIcon="checkmark"
+        >
+        </Collapse>
+        <Collapse icon="apple" :title="$t('StrApple')" statusIcon="checkmark">
+        </Collapse>
+        <Collapse icon="google" :title="$t('StrGoogle')" statusIcon="checkmark">
+        </Collapse>
+        <Collapse
+          icon="microsoft"
+          :title="$t('StrMicrosoft')"
+          statusIcon="checkmark"
+        >
+        </Collapse>
+        <Collapse
+          icon="facebook"
+          :title="$t('StrFacebook')"
+          statusIcon="checkmark"
+        >
+        </Collapse>
+        <Collapse
+          icon="instagram"
+          :title="$t('StrInstagram')"
+          statusIcon="checkmark"
+        >
+        </Collapse>
+        <Collapse
+          icon="twitter"
+          :title="$t('StrTwitter')"
+          statusIcon="checkmark"
+        >
+        </Collapse>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
