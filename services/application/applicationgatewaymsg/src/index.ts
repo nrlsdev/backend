6a917cd0e78b3abc242c3c97973d90198d0354f2
Raphael Bühringer <raphael.buehringer@revanced.com>
2021-03-16 17:13:40 +0100

    implement socket.io websocket server

--- services/application/applicationgatewaymsg/src/index.ts
+++ services/application/applicationgatewaymsg/src/index.ts
@@ -1,3 +1,28 @@
-import { startWebSocketServer } from './websocket/websocket';
+import { Server as SocketIOServer, Socket } from 'socket.io';
+import { Server } from '@backend/server';
+import { Logger } from '@backend/logger';
 
-startWebSocketServer();
+const logger: Logger = new Logger('applicationgatewaymsg-index');
+const messageServer: Server = new Server('192.168.178.36', 8086, true);
+const webSocketServer = new SocketIOServer(messageServer.Server);
+
+webSocketServer.on('connection', onWebSocketServerConnection);
+
+function onWebSocketServerConnection(client: Socket) {
+  logger.info('connection', 'Client connected.');
+
+  client.on('message', onWebSocketServerMessage);
+
+  client.on('disconnect', onWebSocketServerDisconnect);
+}
+
+function onWebSocketServerMessage(message: string) {
+  logger.info('message', 'Message received');
+  console.log(message);
+}
+
+function onWebSocketServerDisconnect() {
+  logger.info('connection', 'Client disconnected.');
+}
+
+messageServer.start();
